524fe57f595e222b085afbc05e04333af250e472
Umar <11750115076@students.uin-suska.ac.id>
2022-11-10 20:26:58 +0700

    init

---
+++ database/migrations/2022_11_10_131335_create_travels_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('quota');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('hotel_name')->nullable();
+            $table->string('price');
+            $table->string('img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}

0798c2f1b9ec161a9e39b2570d70fd8ce1bf053d
Umar <11750115076@students.uin-suska.ac.id>
2022-11-13 14:31:57 +0700

    sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -2,28 +2,28 @@
     <div class="pcoded-inner-navbar main-menu">
         <div class="pcoded-navigatio-lavel">Navigation</div>
         <ul class="pcoded-item pcoded-left-item">
-            <li class="active">
+            <li class="{{ Request::is('dashboard') ? "active" : ""}}">
                 <a href="javascript:void(0)">
                     <span class="pcoded-micon"><i class="feather icon-home"></i></span>
                     <span class="pcoded-mtext">Dashboard</span>
                 </a>
             </li>
             {{-- Karyawan --}}
-            <li class="">
+            <li class="{{ Request::is('travel') ? "active" : ""}}">
                 <a href="{{ route('travel.index') }}">
                     <span class="pcoded-micon"><i class="feather icon-menu"></i></span>
                     <span class="pcoded-mtext">Wisata</span>
                 </a>
             </li>
             {{-- Admin --}}
-            <li class="">
-                <a href="navbar-light.htm">
+            <li class="{{ Request::is('order') ? "active" : ""}}">
+                <a href="{{ route('order.index') }}">
                     <span class="pcoded-micon"><i class="feather icon-menu"></i></span>
                     <span class="pcoded-mtext">Pesanan</span>
                 </a>
             </li>
-            <li class="">
-                <a href="navbar-light.htm">
+            <li class="{{ Request::is('user') ? "active" : ""}}">
+                <a href="{{ route('user.index') }}">
                     <span class="pcoded-micon"><i class="feather icon-menu"></i></span>
                     <span class="pcoded-mtext">Karyawan</span>
                 </a>
